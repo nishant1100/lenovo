8a0cfb7999ec6f96616310a95b135de31f1755a6
unknown <nishantstha1010@gmail.com>
2022-08-11 11:46:58 +0545

    hello

---
+++ newfile.py
@@ -0,0 +1,11 @@
+# ....>It represent a one line comment in python
+
+'''
+It represnet a multi-line comment 
+We can write any thing here 
+'''
+  
+a = 5 #here , a is a variable which store/holds integer type value i.e.5
+name = "name" #here name is # variable which stores/holds string type value i.e. "name"
+# Add in python string are always inside or double quotes:
+# For example: "mane" OR 'name'
